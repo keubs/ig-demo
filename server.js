7c272ece3dc245b534cfe363adf68b571f114859
Kevin Cook <kevinac4@gmail.com>
2020-04-22 23:15:31 -0700

    no flash

--- server.js
+++ server.js
@@ -85,9 +85,9 @@ app.use(
 
 // Global vars
 app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+  // res.locals.success_msg = req.flash('success_msg');
+  // res.locals.error_msg = req.flash('error_msg');
+  // res.locals.error = req.flash('error');
   res.locals.user = req.user || null;
   next();
 });
